4ebed2cb8b85802ed5589228311d38e5b2c35d40
SihyeonHong <hongsihyeon179@gmail.com>
2024-02-27 13:02:53 +0900

    Header & Footer

--- bookstore-cra/src/components/common/Footer.tsx
+++ bookstore-cra/src/components/common/Footer.tsx
@@ -1,8 +1,39 @@
+import styled from "styled-components";
+import logo from "../../assets/images/logo.png";
+
 export const Footer = () => {
   return (
-    <>
-      <hr />
-      <div>copyright(c), 2024, book store.</div>
-    </>
+    <FooterStyle>
+      <h1 className="logo">
+        <img src={logo} alt="book store logo" />
+      </h1>
+      <div className="copyright">
+        <p>copyright(c), 2024, book store.</p>
+      </div>
+    </FooterStyle>
   );
 };
+
+const FooterStyle = styled.footer`
+  width: 100%;
+  margin: 0 auto; // 가운데정렬
+  max-width: ${({ theme }) => theme.layout.width.large};
+  border-top: 1px solid ${({ theme }) => theme.color.background};
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    img {
+      width: 140px;
+    }
+  }
+
+  .copyright {
+    p {
+      font-size: 0.75rem;
+      color: ${({ theme }) => theme.color.text};
+    }
+  }
+`;
--- bookstore-cra/src/components/layout/Layout.tsx
+++ bookstore-cra/src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import { Footer } from "../common/Footer";
 import { Header } from "../common/Header";
+import styled from "styled-components";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,8 +11,15 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <LayoutStyle>{children}</LayoutStyle>
       <Footer />
     </>
   );
 };
+
+const LayoutStyle = styled.main`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding: 20px 0;
+`;
